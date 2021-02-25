b7589bc465a665c7bf5fa3f784ba5d4d072c1b7b
seo-dong-hyeon <62382430+seo-dong-hyeon@users.noreply.github.com>
2021-02-26 06:22:51 +0900

    add author field in Todo

--- mysite/todos/models.py
+++ mysite/todos/models.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.models import User
 from django.db import models
+
 
 class Todo(models.Model):
     # id
@@ -7,6 +9,7 @@ class Todo(models.Model):
     is_completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
--- mysite/todos/views.py
+++ mysite/todos/views.py
@@ -1,5 +1,5 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from .models import Todo
 from .form import TodoForm
 from django.utils import timezone
@@ -17,12 +17,14 @@ def detail(request, todo_id):
     return render(request, 'todos/todo_detail.html', context)
 
 
+@login_required(login_url='user:login')
 def todo_create(request):
     if request.method == 'POST':
         form = TodoForm(request.POST)
         if form.is_valid():
             todo = form.save(commit=False)
             todo.created_at = timezone.now()
+            todo.author = request.user
             todo.save()
             return redirect('todos:index')
     else:
