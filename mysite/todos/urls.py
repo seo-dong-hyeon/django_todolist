a55d6e5630a3e5983c4ea7390ee90f6e19735d11
seo-dong-hyeon <62382430+seo-dong-hyeon@users.noreply.github.com>
2021-02-25 22:24:09 +0900

    add views.index, views.detail

--- mysite/todos/urls.py
+++ mysite/todos/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'todos'
+
 urlpatterns = [
     path('', views.index),
+    path('<int:todo_id>/', views.detail, name='detail'),
 ]
--- mysite/todos/views.py
+++ mysite/todos/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Todo
+
 
 def index(request):
-    return HttpResponse("todos")
+    todos = Todo.objects.all()
+    context = {'todos': todos}
+    return render(request, 'todos/todos.html', context)
+
+
+def detail(request, todo_id):
+    todo = Todo.objects.get(id=todo_id)
+    context = {'todo': todo}
+    return render(request, 'todos/todo_detail.html', context)
